fe02f7c5af0524b818ddcb25850e2096f4a4ca31
divakotova <divakova.dasha@yandex.by>
2023-06-19 21:52:46 +0300

    feat: implement cli

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,11 @@
 const parseEnv = () => {
-    // Write your code here 
+    const values = [];
+    for (let key of Object.keys(process.env)) {
+      if (key.slice(0, 4) === "RSS_") {
+        key.push(`${key}=${process.env[key]}`);
+      }
+    }
+    console.log(values.join("; "));
 };
 
 parseEnv();
