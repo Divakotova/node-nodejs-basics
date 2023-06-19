f21bf6e508c254d525e60d8f45a90b94fdd15083
divakotova <divakova.dasha@yandex.by>
2023-06-19 22:41:48 +0300

    feat: implement hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,18 @@
+import path from "path";
+import { createHash } from "crypto";
+import { fileURLToPath } from "url";
+import { readFile } from "fs/promises";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const calculateHash = async () => {
-    // Write your code here 
+  const content = await readFile(
+    path.join(__dirname, "files", "fileToCalculateHashFor.txt")
+  );
+  const hash = createHash("sha256").update(content).digest("hex");
+
+  console.log(hash);
 };
 
 await calculateHash();
