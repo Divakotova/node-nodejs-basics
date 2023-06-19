ae397482b3af0a45b83eecc72541ab4de3250fef
divakotova <divakova.dasha@yandex.by>
2023-06-19 22:58:30 +0300

    feat: implement zip

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,17 @@
+import fs from "fs";
+import path from "path";
+import zlib from "zlib";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const compress = async () => {
-    // Write your code here 
+  const file = path.join(__dirname, "files", "fileToCompress.txt");
+  const archiveFile = path.join(__dirname, "files", "archive.gz");
+  const readableStream = fs.createReadStream(file);
+  const writableStream = fs.createWriteStream(archiveFile);
+  readableStream.pipe(zlib.createGzip()).pipe(writableStream);
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,18 @@
+import fs from "fs";
+import path from "path";
+import zlib from "zlib";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const decompress = async () => {
-    // Write your code here 
+  const file = path.join(__dirname, "files", "fileToCompress.txt");
+  const archiveFile = path.join(__dirname, "files", "archive.gz");
+  const readableStream = fs.createReadStream(archiveFile);
+  const writableStream = fs.createWriteStream(file);
+
+  readableStream.pipe(zlib.createGunzip()).pipe(writableStream);
 };
 
 await decompress();
