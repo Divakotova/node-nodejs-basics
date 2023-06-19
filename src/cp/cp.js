42e90a17f1111d4a83cda2d14a41810ecd8dc20a
divakotova <divakova.dasha@yandex.by>
2023-06-19 22:54:50 +0300

    feat: implement cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,26 @@
+import path from "path";
+import cp from "child_process";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const file = path.join(__dirname, "files", "script.js");
+  const node = cp.spawn(`node`, [file].concat(args));
+
+  node.on("data", (data) => process.stdout.write(data));
+  node.on("error", (err) => {
+    console.log(`error: ${err.message}`);
+  });
+  node.on("close", (code) => {
+    console.log(`childe process closed with code: ${code}`);
+    process.exit();
+  });
+
+  node.stdout.pipe(process.stdin);
+  process.stdout.pipe(node.stdin);
+  А;
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(["someArgument1", "someArgument2", "someArgument3"]);
